1b76f1b56c5a1f88115c42d09ed73e1cc415819f
Joao Pedro Gonzatti <joaopedro.gonzatti@hotmail.com>
2025-06-07 17:47:48 -0300

    add comment

--- wpai-fix-emoji-import.php
+++ wpai-fix-emoji-import.php
@@ -2,8 +2,10 @@
 /**
  * Plugin Name: WP All Import - Fix emoji import
  * Version: 1.0
+ * This plugin disables the WP All Import emoji import filter.
 */
 
+// Disables the stream filter to allow emojis
 add_filter('wp_all_import_is_enabled_stream_filter', 'wpai_wp_all_import_is_enabled_stream_filter', 10, 1);
 function wpai_wp_all_import_is_enabled_stream_filter($filter_is_active) {
     $filter_is_active = false;
@@ -11,6 +13,7 @@ function wpai_wp_all_import_is_enabled_stream_filter($filter_is_active) {
     return $filter_is_active;
 }
 
+// Disables the filter that removes non-ASCII characters from CSV to XML conversion
 add_filter('wp_all_import_csv_to_xml_remove_non_ascii_characters', 'wpai_wp_all_import_csv_to_xml_remove_non_ascii_characters', 10, 1);
 function wpai_wp_all_import_csv_to_xml_remove_non_ascii_characters($filter_is_active) {
     $filter_is_active = false;
